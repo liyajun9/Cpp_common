bc02c4b3dde9cfd68949d9cf562ce13af2d1ea0a
liyajun9 <liyajun9@126.com>
2023-09-12 11:27:48 +0800

    update gitignore

--- scp2compilecenter.sh
+++
@@ -1,2 +0,0 @@
-scp -q -r -P 22 CMakeLists.txt ralph@192.168.131.193:/home/ralph/CLionProjects/compile_center/cpp_common/
-scp -q -r -P 22 src ralph@192.168.131.193:/home/ralph/CLionProjects/compile_center/cpp_common/
